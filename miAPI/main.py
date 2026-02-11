494217be8353a4a6ef93f4611cc4d9f130d5c2f6
EduardoBA-05 <123046652@upq.edu.mx>
2026-02-10 22:59:37 -0600

    Práctica 2 - Endpoints con parámetros obligatorios y opcionales - TERMINADA
    Creación de Endpoints para visualizar el funcionamiento de los parámetros, así como ver la documentación de Swagger UI.

--- miAPI/main.py
+++ miAPI/main.py
@@ -15,3 +15,18 @@ async def bienvenido():
     await asyncio.sleep(5)
     return {"mensaje":"Bienvenido Mundo FastAPI",
             "estatus":"200"}
+
+# Endpoint con parámetro obligatorio
+@app.get("/programador") 
+async def programador(nombre: str): 
+    if not nombre.strip(): # revisa si está vacío o solo espacios 
+        return {"mensaje": "No especificaste tu nombre"}
+        return {"mensaje": f"Hola programador {nombre}, bienvenido a la API"}
+
+#Endpoint con parámetros opcionales
+@app.get("/usuario")
+async def usuario(nombre: str = "Invitado", edad: int | None = None):
+    if edad:
+        return {"mensaje": f"Hola {nombre}, tu edad es {edad}"}
+    return {"mensaje": f"Hola {nombre}, no especificaste edad"}
+
